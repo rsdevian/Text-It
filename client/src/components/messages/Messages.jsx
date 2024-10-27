b90dadda03f1619b0b4a83eac16cc79991372bfc
Vaishnav Nishanth A V <92431643+vaishnavnishanth3@users.noreply.github.com>
2024-10-27 10:41:24 +0530

    Update Messages.jsx

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -14,12 +14,12 @@ function Messages() {
 
   return (
     <div className="px-4 flex-1 overflow-auto">
-        {!loading && messages.length > 0 && messages.map((message) => (<Message key={message._id} message={message}/>))}
+        {!loading && messages.length > 0 && <div>messages.map((message) => (<Message key={message._id} message={message}/>))} <div ref={lastMessageRef}/></div>
         {loading && [...Array(3)].map((_,idx) => <MessageSkeleton key={idx}/>)}
         {!loading && messages.length === 0 && (
           <p className="text-center">Send a message to start the conversation</p>
         )}
-      <div ref={lastMessageRef}/>
+        
     </div>
   )
 }
